fd4fdc70503e0ee9cda774c1addf0105226e40e8
harunkkhan <harunkkhan1@gmail.com>
2025-04-24 16:19:21 -0400

    udpate api

--- api/api.py
+++ api/api.py
@@ -24,7 +24,7 @@ CORS(app, origins=[
     # Add your production domain here if you have one, e.g.:
     # "https://your-production-domain.com", 
     "http://localhost:5173",
-    https://pay-tracker.vercel.app/
+    https://pay-tracker.vercel.app
 ], supports_credentials=True) # Add supports_credentials=True if needed, and ensure regex=True is default or set
 
 #initialize backend components
