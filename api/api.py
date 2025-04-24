5666c77cde991681ccc59835819e5af3b7bd2703
Hailey Pan <josephtso914@gmail.com>
2025-04-23 23:13:41 -0400

    port fixes

--- api/api.py
+++ api/api.py
@@ -145,4 +145,5 @@ def get_categories():
     return jsonify({})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
